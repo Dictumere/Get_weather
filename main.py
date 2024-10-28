7066c4703d8e53f932de8757833becf5cb490783
Dictumere <118251469+Dictumere@users.noreply.github.com>
2024-10-28 19:08:17 +0300

    Correct_attributes
    Изменил, чтобы все атрибуты payload применялись ко всем локациям, а не только к Череповцу

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import requests
 
 
-def determine_weather(article_id, lang, use_params):
+def determine_weather(article_id):
     url_template = 'https://wttr.in/{}?nTqu3%20HTTP/1.1'
     url = url_template.format(article_id)
-    if use_params:
-        payload = {'lang': lang, 'M': '', 'n': '', 'q': '', 'T': '', '3': ''}
-    else:
-        payload = {}
+    payload = {'lang': 'ru', 'M': '', 'n': '', 'q': '', 'T': '', '3': ''}
     response = requests.get(url, params=payload)
     response.raise_for_status()
     return response.text
 
 
 if __name__ == "__main__":
-    articles_id = [('London', 'en', False),
-                   ('svo', 'en', False),
-                   ('Череповец', 'ru', True)]
-    for article_id, lang, use_params in articles_id:
-        weather = determine_weather(article_id, lang, use_params)
+    articles_id = [('Лондон'),
+                   ('svo'),
+                   ('Череповец')]
+    for article_id in articles_id:
+        weather = determine_weather(article_id)
         print(weather)
