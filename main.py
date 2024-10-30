c97416b7ca1870e227efdb55be171844e36de7ba
Dictumere <118251469+Dictumere@users.noreply.github.com>
2024-10-30 09:10:58 +0300

    Edit name in code and add file dependencies

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 
 
-def determine_weather(article_id):
-    url_template = 'https://wttr.in/{}?nTqu3%20HTTP/1.1'
-    url = url_template.format(article_id)
+def determine_weather(location):
+    url_template = 'https://wttr.in/{}?'
+    url = url_template.format(location)
     payload = {'lang': 'ru', 'M': '', 'n': '', 'q': '', 'T': '', '3': ''}
     response = requests.get(url, params=payload)
     response.raise_for_status()
@@ -11,9 +11,9 @@ def determine_weather(article_id):
 
 
 if __name__ == "__main__":
-    articles_id = [('Лондон'),
-                   ('svo'),
-                   ('Череповец')]
-    for article_id in articles_id:
-        weather = determine_weather(article_id)
+    locations = [('Лондон'),
+                 ('svo'),
+                 ('Череповец')]
+    for location in locations:
+        weather = determine_weather(location)
         print(weather)
